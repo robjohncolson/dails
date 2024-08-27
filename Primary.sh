f11172b46a0ea3944f1a72c37c779a94c1786399
robjohncolson <robjohncolson@protonmail.com>
2024-08-27 12:03:36 +0000

    added url and reworded comments in the primary script.

--- Primary.sh
+++ Primary.sh
@@ -2,6 +2,8 @@
 #Primary script
 #Create a secure OS with minimal software.
 #This script will run all secondary scripts: A, B, C, etc.
+#Those will run their respective tertiary scripts: A1, A2, A3, etc.
+#and so on.
 
 # Run secondary script A
 ./SecondaryA.sh
