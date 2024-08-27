257cab7b17a0ff5cdb55f12f098f8eab0c2d34cf
robjohncolson <robjohncolson@protonmail.com>
2024-08-27 14:04:54 +0000

    Added a python script for getting info from lsblk.

---
+++ TertiaryA1a1.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+# tertiary script A1a1: python script gets info from lsblk and writes to file.
+
+import subprocess
+
+def get_block_device_info():
+    # Get information about all block devices
+    lsblk_output = subprocess.check_output(['lsblk', '-o', 'NAME,SIZE,TYPE,MOUNTPOINT']).decode('utf-8')
+    return lsblk_output
+
+def write_to_file(info):
+    # Write the information to a file
+    with open('block_device_info.txt', 'w') as file:
+        file.write(info)
+
+if __name__ == "__main__":
